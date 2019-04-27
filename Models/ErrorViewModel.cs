379d8867342553cb62d21a7c222c9ff7b91cbe3c
Savchuk <123tachka123@gmail.com>
2019-04-27 12:16:43 +0300

    dz trpz

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Новая_папка__2_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
